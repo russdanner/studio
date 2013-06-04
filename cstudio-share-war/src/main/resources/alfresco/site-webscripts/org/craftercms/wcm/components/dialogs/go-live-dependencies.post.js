f27e3b62efb8407d63ba04fe67ae7fb91003ad29
Dejan Brkic <dbrkic@rivetlogic.com>
2013-06-04 15:56:56 -0600

    Modified go live dialog to show dialog immediately and than populate items and dependencies. Previous implementation was showing dialog after all items and dependencies were populated, which was leaving user waiting long time when submitting large set of data.

--- cstudio-share-war/src/main/resources/alfresco/site-webscripts/org/craftercms/wcm/components/dialogs/go-live-dependencies.post.js
+++ cstudio-share-war/src/main/resources/alfresco/site-webscripts/org/craftercms/wcm/components/dialogs/go-live-dependencies.post.js
@@ -1,16 +1,14 @@
 var site = args.site;
 var myTicket = cstudioApplicationService.currentApplicationContext().getTicket();
 if (site == undefined || site == '') {
-	status.code = 400;
-	status.message = "Site must be provided.";
-	status.redirect = true;
+    status.code = 400;
+    status.message = "Site must be provided.";
+    status.redirect = true;
 } else {
-	var connector = remote.connect("alfresco");
-	var resultJson = connector.post("/cstudio/wcm/dependency/get-dependencies?site="+site, requestbody.content, "text/xml");
-	var resultChannels = connector.post("/cstudio/publish/get-available-publishing-channels?site=" + site, requestbody.content, "application/json");
+    var connector = remote.connect("alfresco");
+    var resultJson = connector.post("/cstudio/wcm/dependency/get-dependencies?site="+site, requestbody.content, "text/xml");
 
-	var result = eval('(' + resultJson + ')');	
+    var result = eval('(' + resultJson + ')');
     model.dependencies = result;
     model.jsonDependencies = resultJson;
-    model.channels = eval('(' + resultChannels + ')');
 }
