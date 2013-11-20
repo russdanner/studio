8a907eae9d9dc3538381963d31d7eaf350b2c990
Dejan Brkic <dejan.brkic@craftersoftware.com>
2013-11-20 14:10:02 -0600

    CRAFTERCMS-427 Fixed bugs causing change template to fail for following use cases: - (content as folder = true) -> (content as folder = false) and vice versa - (file-name) -> (autofile-name) and vice versa Additionally fixed bug with content with autofile-name control not respecting content-as-folder configuration (always creating content as folder)

--- cstudio-share-war/src/main/webapp/components/cstudio-forms/controls/auto-filename.js
+++ cstudio-share-war/src/main/webapp/components/cstudio-forms/controls/auto-filename.js
@@ -1,38 +1,39 @@
-CStudioForms.Controls.AutoFilename = CStudioForms.Controls.AutoFilename ||  
-function(id, form, owner, properties, constraints)  {
-	var _self = this;
-	this.owner = owner;
-	this.owner.registerField(this);
-	this.errors = []; 
-	this.properties = properties;
-	this.constraints = constraints;
-	this.inputEl = null;
-	this.countEl = null;
-	this.required = true;
-	this.value = "_not-set";
-	this.form = form;
-	this.id = "file-name";
-	this.contentAsFolder = true;
-		
-	return this;
-}
+CStudioForms.Controls.AutoFilename = CStudioForms.Controls.AutoFilename ||
+    function(id, form, owner, properties, constraints)  {
+        var _self = this;
+        this.owner = owner;
+        this.owner.registerField(this);
+        this.errors = [];
+        this.properties = properties;
+        this.constraints = constraints;
+        this.inputEl = null;
+        this.countEl = null;
+        this.required = true;
+        this.value = "_not-set";
+        this.form = form;
+        this.id = "file-name";
+        this.contentAsFolder = (form.definition) ? form.definition.contentAsFolder : null;
+
+        return this;
+    }
 
 YAHOO.extend(CStudioForms.Controls.AutoFilename, CStudioForms.CStudioFormField, {
 
     getLabel: function() {
         return "Auto Filename";
     },
-    
-	render: function(config, containerEl) {
-		// this widget has no visual presentation
-	},
 
-	getValue: function() {
-		return this.value;
-	},
+    render: function(config, containerEl) {
+        // this widget has no visual presentation
+    },
+
+    getValue: function() {
+        return this.value;
+    },
 
     setValue: function(value) {
         var filename = value;
+        var changeTemplate = CStudioAuthoring.Utils.getQueryVariable(location.search, "changeTemplate");
 
         if(filename == "") {
             // if value has not been set, use the item's object ID as the filename
@@ -45,26 +46,28 @@ YAHOO.extend(CStudioForms.Controls.AutoFilename, CStudioForms.CStudioFormField, 
             this.form.updateModel("file-name", "index.xml");
             this.form.updateModel("folder-name", this.form.model["objectId"]);
             //this.value = "index.xml";
-        }
-        else {
+        } else if (changeTemplate == "true") {
+            this.form.updateModel("file-name", this.form.model["objectId"] + ".xml");
+            obj.form.updateModel("folder-name", "");
+        } else {
             this.form.updateModel("file-name", filename);
             obj.form.updateModel("folder-name", "");
         }
     },
 
-	getName: function() {
-		return "auto-filename";
-	},
-	
-	getSupportedProperties: function() {
-		return [
-			];
-	},
+    getName: function() {
+        return "auto-filename";
+    },
 
-	getSupportedConstraints: function() {
-		return [ // required is assumed
-		];
-	}
+    getSupportedProperties: function() {
+        return [
+        ];
+    },
+
+    getSupportedConstraints: function() {
+        return [ // required is assumed
+        ];
+    }
 
 });
 
