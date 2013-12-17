russdanner/studio

commit 5f0908ba3d2c606313f07ca65307344cffe44cfe

CRAFTERCMS-402: Change template leaves fields from previous content type
The "Change Template" operation will not remove any fields
1) defined in forms
2) system fields (hard-coded in js): objectGroupId, objectId,
   folder-name, createdDate & lastModifiedDate
3) any fields defined as customFields (see below) in config.xml of
   each content type. removeOnChangeType = false means that the system
   should not remove these fields on change template.

<customFields>
  <field>
    <name>NAME</name>
    <removeOnChangeType>false</removeOnChangeType>
  </field>
</customFields>

Also, the user is warned about the possibility of data loss before
carrying out the template change.

cstudio-share-war/src/main/webapp/components/cstudio-forms/forms-engine.js
@@ -616,8 +616,11 @@ var CStudioForms = CStudioForms || function() {
 
             var formDefinition = CStudioForms.Util.loadFormDefinition(formId, {
                 success: function(formDef) {
-                    CStudioForms.Util.LoadFormController(formId, {
-                        success: function(customControllerClass) {
+                    CStudioForms.Util.LoadFormConfig(formId, {
+                        success: function(customControllerClass, formConfig) {
+
+                            formDef.config = formConfig;    // Make the form configuration available in the form definition
+
                             var path = CStudioAuthoring.Utils.getQueryVariable(location.search, "path");
                             if( path && path.indexOf(".xml") != -1) {
                                 //Check if the form is locked
@@ -1877,16 +1880,16 @@ var CStudioForms = CStudioForms || function() {
 
 
         /**
-         * Load the form field controller
+         * Load the form field controller and form configuration
          */
-        LoadFormController: function(formId, cb) {
+        LoadFormConfig: function(formId, cb) {
             var configCb = {
-                success: function(config) {
+                success: function(formConfig) {
 
-                    if(config['controller'] && config['controller'] == "true") {
+                    if(formConfig['controller'] && formConfig['controller'] == "true") {
                         var moduleCb = {
                             moduleLoaded: function(moduleName, clazz, config) {
-                                cb.success(clazz);
+                                cb.success(clazz, formConfig);
                             },
                             failure: function() {
                                 cb.failure();
@@ -1902,7 +1905,7 @@ var CStudioForms = CStudioForms || function() {
                             moduleConfig, moduleCb);
                     }
                     else {
-                        cb.success();
+                        cb.success(undefined, formConfig);
                     }
                 },
                 failure: function() {
@@ -2049,58 +2052,87 @@ var CStudioForms = CStudioForms || function() {
                 }
             }
 
-            for (var key in form.model) {
-
-                // "content-type","display-template" and "merge-strategy" will appear as part of the model
-                // after the form has been saved for the first time.
-                // To prevent duplicates values in the model
-                if (key == "content-type" || key ==  "display-template" || key ==  "merge-strategy") {
-                    continue;
-                }
-
-                var modelItem = form.model[key];
-
-                if(Object.prototype.toString.call( modelItem ) === '[object Array]') {
-                    xml += "\t<"+key+">";
-                    for(var j=0; j<modelItem.length; j++) {
-                        xml += "\t<item>";
-                        var repeatItem = modelItem[j];
-                        for (var repeatKey in repeatItem) {
-                            var repeatValue = modelItem[j][repeatKey];
-
-                            xml += "\t<"+repeatKey+">";
-                            if(Object.prototype.toString.call( repeatValue ).indexOf('[object Array]') != -1) {
-                                for (var k=0; k<repeatValue.length; k++) {
-                                    var subModelItem = repeatValue[k];
-                                    xml += "\t\t<item>";
-                                    for (var subRepeatKey in subModelItem) {
-                                        var subRepeatValue = subModelItem[subRepeatKey];
-                                        xml += "<"+subRepeatKey+">";
-                                        xml +=  this.escapeXml(subRepeatValue);
-                                        xml += "</"+subRepeatKey+">\r\n";
-                                    }
-                                    xml += "\t\t</item>";
-                                }
-                            }
-                            else {
-                                xml +=  this.escapeXml(repeatValue);
-                            }
-                            xml += "</"+repeatKey+">\r\n";
-                        }
-                        xml += "\t</item>";
-                    }
-                    xml += "</"+key+">\r\n";
-                }
-                else {
-                    xml += "\t<"+key+">";
-                    xml += this.escapeXml(modelItem);
-                    xml += "</"+key+">\r\n";
-                }
-            }
-
+            xml += this.printFieldsToXml(form.model, form.sections, form.definition.config);
             xml += "</"+form.definition.objectType+">";
 
             return xml;
+        },
+
+        printFieldsToXml: function(formModel, formSections, formConfig) {
+            var validFields = ['$!', 'objectGroupId', 'objectId', 'folder-name', 'createdDate', 'lastModifiedDate'],
+                output = '',
+                validFieldsStr, fieldRe, section;
+
+            // Add valid fields from form sections
+            for (var i = formSections.length - 1; i >= 0; i--) {
+                section = formSections[i];
+
+                for (var j = section.fields.length - 1; j >= 0; j--)
+                validFields.push(section.fields[j].id);
+            }
+
+            // Add valid fields from form config
+            if (formConfig && formConfig.customFields) {
+                for (var i in formConfig.customFields) {
+                    if (formConfig.customFields.hasOwnProperty(i)) {
+                        if (formConfig.customFields[i].removeOnChangeType == 'false') {
+                            validFields.push(formConfig.customFields[i].name);
+                        }
+                    }
+                }
+            }
+
+            validFields.push('$!');     // End element
+            validFieldsStr = validFields.join(",");
+
+            for (var key in formModel) {
+
+                // Because we added start and end elements, we can be sure that any field names will
+                // be delimited by the delimiter token (ie. comma)
+                fieldRe = new RegExp("," + key + "(?:,|\|\d+\|)", "g");
+
+                if (fieldRe.test(validFieldsStr)) {
+                    var modelItem = formModel[key];
+
+                    if(Object.prototype.toString.call( modelItem ) === '[object Array]') {
+                        output += "\t<"+key+">";
+                        for(var j = 0; j < modelItem.length; j++) {
+                            output += "\t<item>";
+                            var repeatItem = modelItem[j];
+                            for (var repeatKey in repeatItem) {
+                                var repeatValue = modelItem[j][repeatKey];
+
+                                output += "\t<"+repeatKey+">";
+                                if(Object.prototype.toString.call( repeatValue ).indexOf('[object Array]') != -1) {
+                                    for (var k = 0; k < repeatValue.length; k++) {
+                                        var subModelItem = repeatValue[k];
+                                        output += "\t\t<item>";
+                                        for (var subRepeatKey in subModelItem) {
+                                            var subRepeatValue = subModelItem[subRepeatKey];
+                                            output += "<"+subRepeatKey+">";
+                                            output +=  this.escapeXml(subRepeatValue);
+                                            output += "</"+subRepeatKey+">\r\n";
+                                        }
+                                        output += "\t\t</item>";
+                                    }
+                                }
+                                else {
+                                    output +=  this.escapeXml(repeatValue);
+                                }
+                                output += "</"+repeatKey+">\r\n";
+                            }
+                            output += "\t</item>";
+                        }
+                        output += "</"+key+">\r\n";
+                    }
+                    else {
+                        output += "\t<"+key+">";
+                        output += this.escapeXml(modelItem);
+                        output += "</"+key+">\r\n";
+                    }
+                }
+            }
+            return output;
         },
 
         escapeXml: function(value) {

cstudio-share-war/src/main/webapp/components/cstudio-contextual-nav/wcm-site-dropdown-mods/wcm-root-folder.js
@@ -1773,48 +1773,93 @@ treeNode.getHtml = function() {
              */
             changeTemplate: function(sType, args, tree) {
 
-                var assignTemplateCb = {
-                    success: function(selectedType) {
-                        var path = (this.activeNode.data.uri);
+                var modalBody = YDom.get("cstudio-wcm-popup-div");
+                if (modalBody === null) {
+                    modalBody = document.createElement("div");
+                    modalBody.setAttribute("id", "cstudio-wcm-popup-div");
+                    document.body.appendChild(modalBody);
+                }
 
-                        var editCb = {
-                            success: function() {
-                                this.callingWindow.location.reload(true);
-                            },
+                var continueFn = function continueFn (e) {
+                    e.preventDefault();
 
-                            failure: function() {
-                            },
+                    var assignTemplateCb = {
+                        success: function(selectedType) {
+                            var path = (this.activeNode.data.uri);
 
-                            callingWindow: window
-                        };
-                        /* reload dashboard is heavy, to reflect changed content-type */
-                        //window.location.reload(true);
-                        //this.activeNode.data.formId = selectedType;
-                        var auxParams = new Array();
-                        var param1 = {};
-                        param1['name'] = "draft";
-                        param1['value'] = "true";
-                        var param2 = {};
-                        param2['name'] = "changeTemplate";
-                        param2['value'] = "true";
-                        auxParams.push(param1);
-                        auxParams.push(param2);
-                        CStudioAuthoring.Operations.editContent(
-                                selectedType,
-                                CStudioAuthoringContext.site,
-                                path,
-                                this.activeNode.data.nodeRef, path, false, editCb,auxParams);
-                    },
-                    failure: function() { },
-                    activeNode: oCurrentTextNode
+                            var editCb = {
+                                success: function() {
+                                    this.callingWindow.location.reload(true);
+                                },
+
+                                failure: function() {
+                                },
+
+                                callingWindow: window
+                            };
+                            /* reload dashboard is heavy, to reflect changed content-type */
+                            //window.location.reload(true);
+                            //this.activeNode.data.formId = selectedType;
+                            var auxParams = new Array();
+                            var param1 = {};
+                            param1['name'] = "draft";
+                            param1['value'] = "true";
+                            var param2 = {};
+                            param2['name'] = "changeTemplate";
+                            param2['value'] = "true";
+                            auxParams.push(param1);
+                            auxParams.push(param2);
+                            CStudioAuthoring.Operations.editContent(
+                                    selectedType,
+                                    CStudioAuthoringContext.site,
+                                    path,
+                                    this.activeNode.data.nodeRef, path, false, editCb,auxParams);
+                        },
+                        failure: function() { },
+                        activeNode: oCurrentTextNode
+                    };
+
+                    CStudioAuthoring.Operations.assignContentTemplate(
+                            CStudioAuthoringContext.site,
+                            CStudioAuthoringContext.user,
+                            oCurrentTextNode.data.uri,
+                            assignTemplateCb,
+                            oCurrentTextNode.data.formId);
                 };
 
-                CStudioAuthoring.Operations.assignContentTemplate(
-                        CStudioAuthoringContext.site,
-                        CStudioAuthoringContext.user,
-                        oCurrentTextNode.data.uri,
-                        assignTemplateCb,
-                        oCurrentTextNode.data.formId);
+                var cancelFn = function cancelFn (e) {
+                    e.preventDefault();
+                    dialog.destroy();
+                }
+
+                modalBody.innerHTML = '<div class="contentTypePopupInner" style="width:460px;height:140px;">' +
+                                        '<div class="contentTypePopupContent">' +
+                                            '<form name="contentFromWCM">' +
+                                            '<div class="contentTypePopupHeader">Warning: Change Content Type</div> ' +
+                                            '<div>The following operation may result in data loss. Would you like to proceed?</div>' +
+                                            '<div class="contentTypePopupBtn">' +
+                                                '<input type="submit" class="ok" id="acceptCTChange" value="Yes" />' +
+                                                '<input type="submit" class="cancel" id="cancelCTChange" value="No" />' +
+                                            '</div>' +
+                                            '</form> ' +
+                                        '</div>' +
+                                      '</div>';
+
+                var dialog = new YAHOO.widget.Dialog("cstudio-wcm-popup-div", 
+                                { fixedcenter : true,
+                                  visible : false,
+                                  modal:false,
+                                  close:false,
+                                  constraintoviewport : true,
+                                  underlay:"none"                                                       
+                                });
+
+                dialog.render();
+
+                YAHOO.util.Event.addListener("acceptCTChange", "click", continueFn);
+                YAHOO.util.Event.addListener("cancelCTChange", "click", cancelFn);
+
+                dialog.show();
             },
             /**
              * given a path, attempt to open the folder to that path

commit 6aeed07fa085404bf0edc95c55291c2abec64636

Allows saving from ICE
Fixes bug introduced by 5f0908ba3d2c606313f07ca65307344cffe44cfe
where the form sections (except for the ICE section) were not being
saved into the xml

cstudio-share-war/src/main/webapp/components/cstudio-forms/forms-engine.js
@@ -2041,7 +2041,7 @@ var CStudioForms = CStudioForms || function() {
                 }
             }
 
-            xml += this.printFieldsToXml(form.model, form.sections, form.definition.config);
+            xml += this.printFieldsToXml(form.model, form.definition.sections, form.definition.config);
             xml += "</"+form.definition.objectType+">";
 
             return xml;